8b20cd3d38cf27080841928cd69fd3cb3a23eb39
maxim-polyakov <maxim7012@mail.ru>
2024-10-02 15:27:06 +0300

    .gitignore

---
+++ Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using DB_package;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            c.Subscribe("Telegram");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                // Prevent the process from terminating.
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        DB_Communication dbc = new DB_Communication();
+                        dbc.insert_to(cr.Value, "messtorage.storage");
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Microsoft.Extensions.Hosting;
+class Program
+{
+    class TelegramBot
+    {
+
+        private ITelegramBotClient bot;
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+                // If serializers are not specified, default serializers from
+                // `Confluent.Kafka.Serializers` will be automatically used where
+                // available. Note: by default strings are encoded as UTF8.
+                using (var p = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    try
+                    {
+                        var dr = await p.ProduceAsync("Telegram", new Message<Null, string> { Value = message.Text });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    }
+                    catch (ProduceException<Null, string> e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        public async Task MainAsync()
+        {
+            bot = new TelegramBotClient("7421411096:AAFXFtgIE0uheM30_9DDGlrup8iaZxAgUek");
+        }
+
+        public static async Task Main(string[] args)
+        {
+            TelegramBot tb = new TelegramBot();
+            tb.MainAsync();
+
+            await Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                var cts = new CancellationTokenSource();
+                var cancellationToken = cts.Token;
+                var receiverOptions = new ReceiverOptions
+                {
+                    AllowedUpdates = { },
+                };
+                tb.bot.StartReceiving(
+                    HandleUpdateAsync,
+                    HandleErrorAsync,
+                    receiverOptions,
+                    cancellationToken
+                );
+            }).RunConsoleAsync();
+        }
+    }
+}
